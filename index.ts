ba0eea31e5cee6e2ee6884a63f327c845bd254a3
tf2_mandeokyi <askminjae@gmail.com>
2020-11-29 12:53:54 +0900

    Modified index.ts

--- index.ts
+++ index.ts
@@ -1,22 +1,53 @@
-import { EduroSurveyApi } from './src/eduroapi/api';
+import { EduroSurveyApi, ParticipantPreview, SurveyUser } from './src/eduroapi/api';
 import * as credentials from './credentials.json';
 
 async function dosurvey() {
-    let user = await EduroSurveyApi.findUser({
-        birthday: credentials.birthday,
-        loginType: 'school',
-        name: credentials.name,
-        orgCode: credentials.orgCode,
-        stdntPNo: null
-    });
+    console.log('로그인 정보: {\n' + 
+        `    생일: ${credentials.birthday},\n` + 
+        `    이름: ${credentials.name},\n` + 
+        `    학교코드: ${credentials.orgCode},\n` + 
+        `    비밀번호: ${''.padStart(credentials.password.length, '*')},\n` + 
+    '}\n');
 
-    if(await user.hasPassword()) {
-        await user.validatePassword(credentials.password);
+    process.stdout.write('로그인 중... ');
+    let user : SurveyUser;
+    try {
+        user = await EduroSurveyApi.findUser({
+            birthday: credentials.birthday,
+            loginType: 'school',
+            name: credentials.name,
+            orgCode: credentials.orgCode,
+            stdntPNo: null
+        });
+
+        if(await user.hasPassword()) {
+            await user.validatePassword(credentials.password);
+        }
+    } catch(e) {
+        console.log('❌');
+        return;
     }
+    console.log('✔️')
 
-    let participant = (await user.getParticipantPreviews())[0];
-    let pinfo = await participant.getParticipantInfo();
-    await pinfo.doSurvey();
+    process.stdout.write('참여자 정보 구하는 중... ');
+    let participant : ParticipantPreview;
+    try {
+        participant = (await user.getParticipantPreviews())[0];
+    } catch(e) {
+        console.log('❌');
+        return;
+    }
+    console.log('✔️')
+    
+    process.stdout.write(`참여자 "${participant.userNameEncpt}" 자가진단하는 중... `);
+    try {
+        let pinfo = await participant.getParticipantInfo();
+        await pinfo.doSurvey();
+    } catch(e) {
+        console.log('❌');
+        return;
+    }
+    console.log('✔️')
 }
 
 dosurvey().then(console.log);
